91d1b514586ec547abd7de0c023670deaf12f6b3
Seelkare <seelkare@ZeroMous>
2016-07-27 21:48:14 +0200

    third push

--- syracuse.go
+++ syracuse.go
@@ -1,39 +1,45 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-func syracuse(x,altitude_max,tps int) (bool,int,int) {
-
-    if x > 0 {
-    if  x == 1 {
-
-        return true,tps,altitude_max
-     } else if x%2 == 0 {
-
-      tps = tps+1
-       if altitude_max <= x {
-       altitude_max = x
-       }
-       return syracuse(x/2,altitude_max,tps)
-
-    } else {
-
-        tps = tps+1
-       if altitude_max <= x {
-       altitude_max = x
-       }
-       return syracuse(3*x+1,altitude_max,tps)
+func max(a, b int) int {
+    _max := a
+    if a < b {
+        _max = b
 
     }
-} else {
-     return false,0,0
+    return _max
 }
+
+
+func syracuse(x, altitude_max, tps int) (bool, int, int) {
+
+	if x > 0 {
+		if x == 1 {
+
+			return true,altitude_max,tps
+		} else if x%2 == 0 {
+
+			tps = tps + 1
+		     altitude_max=max(altitude_max,x)
+			return syracuse(x/2, altitude_max, tps)
+
+		} else {
+
+			tps = tps + 1
+			altitude_max=max(altitude_max,x)
+			return syracuse(3*x+1, altitude_max, tps)
+
+		}
+	} else {
+		return false, 0, 0
+	}
 
 }
 
 func main() {
-    var altitude_max,tps int
-    fmt.Println(syracuse(127,altitude_max,tps))
+	var altitude_max, tps int
+	fmt.Println(syracuse(127, altitude_max, tps))
 }
