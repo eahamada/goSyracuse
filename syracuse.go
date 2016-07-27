0390651a2b47c1ff798591e5feb938b0ac9c3988
eahamada <elarif.ahamada@gmail.com>
2016-07-28 00:47:28 +0200

    Update syracuse.go

--- syracuse.go
+++ syracuse.go
@@ -1,45 +1,53 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func max(a, b int) int {
-    _max := a
-    if a < b {
-        _max = b
+	_max := a
+	if a < b {
+		_max = b
+	}
+	return _max
+}
+func _syracuse(x, u0, _tempsVol, _tempsVolAltitude, _altitudeMax int, _tvaFound bool) (tempsVol, tempsVolAltitude, altitudeMax int) {
+	fmt.Println(x, u0, _tempsVol, _tempsVolAltitude, _altitudeMax)
+	if x == 1 {
+		return _tempsVol, _tempsVolAltitude, _altitudeMax
+	} else {
+		if !_tvaFound && x <= u0 && _tempsVol > 0 {
+			_tempsVolAltitude = max(0, _tempsVol-1)
+			_tvaFound = true
+		}
+		_tempsVol += 1
+		_altitudeMax = max(x, _altitudeMax)
 
-    }
-    return _max
+		if x%2 == 0 {
+			return _syracuse(x/2, u0, _tempsVol, _tempsVolAltitude, _altitudeMax, _tvaFound)
+		} else {
+			return _syracuse(3*x+1, u0, _tempsVol, _tempsVolAltitude, _altitudeMax, _tvaFound)
+		}
+	}
 }
 
-
-func syracuse(x, altitude_max, tps int) (bool, int, int) {
-
-	if x > 0 {
-		if x == 1 {
-
-			return true,altitude_max,tps
-		} else if x%2 == 0 {
-
-			tps = tps + 1
-		     altitude_max=max(altitude_max,x)
-			return syracuse(x/2, altitude_max, tps)
-
-		} else {
-
-			tps = tps + 1
-			altitude_max=max(altitude_max,x)
-			return syracuse(3*x+1, altitude_max, tps)
-
-		}
-	} else {
-		return false, 0, 0
+func syracuse(x int) (tempsVol, tempsVolAltitude, altitudeMax int, err error) {
+	if x < 1 {
+		return -1, -1, -1, errors.New("x doit etre strictement positif")
 	}
-
+	if x == 1 {
+		return 0, 3, 4, nil
+	}
+	_tempsVol, _tempsVolAltitude, _altitudeMax := _syracuse(x, x, 0, 0, x, false)
+	return _tempsVol, _tempsVolAltitude, _altitudeMax, nil
 }
 
 func main() {
-	var altitude_max, tps int
-	fmt.Println(syracuse(127, altitude_max, tps))
+	tempsVol, tempsVolAltitude, altitudeMax, err := syracuse(15)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(tempsVol, tempsVolAltitude, altitudeMax)
+	}
 }
